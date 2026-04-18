f38a069198154c08bcf9d26b0034d503f769caa8
Ryan Cassidy <RyanCassidy@my.unt.edu>
2026-04-18 02:24:38 -0500

    displaying user profile

--- profilesapp/src/main.jsx
+++ profilesapp/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <React.StrictMode>
+ <Authenticator>
+ <App />
+ </Authenticator>
+ </React.StrictMode>
+);
